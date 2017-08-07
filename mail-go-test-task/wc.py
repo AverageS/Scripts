170df9494cdeea2c9d02e378d6e76223e7b4fd77
mikhail <mandarson@gmail.com>
2017-08-07 10:01:01 +0300

    init commit

---
+++ mail-go-test-task/wc.py
@@ -0,0 +1,12 @@
+import sys
+
+args = sys.argv
+if len(args) < 2:
+    raise RuntimeError('You did not Enter filepath. Right usage is python3 [filepath]')
+filename = args[-1]
+
+with open(filename, 'r') as fp:
+    num_lines = sum(1 for line in fp)
+print(num_lines)
+
+
