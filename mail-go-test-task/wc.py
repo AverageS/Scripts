a2c054c4b7ed05060b26c8e3978eeaf61d815554
Mikhail <mandarson@gmail.com>
2017-08-07 13:59:21 +0300

    Update wc.py
    Update help

--- mail-go-test-task/wc.py
+++ mail-go-test-task/wc.py
@@ -2,7 +2,7 @@ import sys
 
 args = sys.argv
 if len(args) < 2:
-    raise RuntimeError('You did not Enter filepath. Right usage is python3 [filepath]')
+    raise RuntimeError('You did not Enter filepath. Right usage is python3 wc.py [filepath]')
 filename = args[-1]
 
 with open(filename, 'r') as fp:
